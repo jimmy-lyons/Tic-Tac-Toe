64ac2548ce4051db1414740c8f2d99e2b24092e4
Jimmy Lyons <jimmylyons1@hotmail.co.uk>
2022-06-22 16:01:32 +0100

    green: each row of tiles contains 3 instances

--- spec/units/board.test.js
+++ spec/units/board.test.js
@@ -2,7 +2,15 @@ const Board = require('../../lib/board.js')
 
 describe('Board', () => {
   it('initialises with 2D array of tile', () => {
-    const board = new Board()
+    const mockTile = null
+    const board = new Board(mockTile)
     expect(board.tiles.length).toEqual(3)
+  })
+  it('each row contains 3 instances', () => {
+    const mockTile = null
+    const board = new Board(mockTile)
+    board.tiles.forEach(
+      row => expect(row.length).toEqual(3)
+    )
   })
 })
