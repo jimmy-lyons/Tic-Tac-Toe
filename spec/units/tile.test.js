c9a7933220474756d57334e3a518a6349d97793b
Jimmy Lyons <jimmylyons1@hotmail.co.uk>
2022-06-22 15:39:54 +0100

    green: setValue changes value of value in Tile

--- spec/units/tile.test.js
+++ spec/units/tile.test.js
@@ -5,4 +5,16 @@ describe('Tile', () => {
     const tile = new Tile()
     expect(tile.value).toBe(null)
   })
+  describe('setValue', () => {
+    it('can change the value of a tile to "O"', () => {
+      const tile = new Tile()
+      tile.setValue('O')
+      expect(tile.value).toBe('O')
+    })
+    it('can change the value of a tile to "X"', () => {
+      const tile = new Tile()
+      tile.setValue('X')
+      expect(tile.value).toBe('X')
+    })
+  })
 })
