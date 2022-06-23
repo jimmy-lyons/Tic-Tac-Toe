d1a1679181f701d0c6152a8732e4eb7a307d10cd
Jimmy Lyons <jimmylyons1@hotmail.co.uk>
2022-06-23 12:51:47 +0100

    green: Game can set value of a tile

--- spec/units/game.test.js
+++ spec/units/game.test.js
@@ -1,26 +1,25 @@
 const Game = require('../../lib/game.js')
+
 const mockTile = jest.createMockFromModule('../../lib/tile.js')
 mockTile.value = jest.fn(() => null)
+mockTile.setValue = jest.fn(() => 'X')
 
-const mockBoard = jest.fn().mockImplementation(() => {
-  return {
-    tiles: [
-      [mockTile, mockTile, mockTile],
-      [mockTile, mockTile, mockTile],
-      [mockTile, mockTile, mockTile]
-    ]
-  }
-})
+const mockBoard = {
+  tiles: [
+    [mockTile, mockTile, mockTile],
+    [mockTile, mockTile, mockTile],
+    [mockTile, mockTile, mockTile]
+  ]
+}
 
 describe('Game', () => {
-  beforeEach (() => {
-    mockTile.mockClear()
-    mockBoard.mockClear()
-  })
   it('initialises with an instance of board', () => {
     const game = new Game(mockBoard)
     expect(game.board).toBe(mockBoard)
-    const tile = game.board().tiles[0][0]
-    expect(tile.value()).toBe(null)
+  })
+  it('can set the value of a tile', () => {
+    const game = new Game(mockBoard)
+    game.makeMove()
+    expect(mockTile.setValue).toHaveBeenCalledTimes(1)
   })
 })
