59ba8433dc16f6925cf383f4bcc0abfd2596e655
Jimmy Lyons <jimmylyons1@hotmail.co.uk>
2022-06-23 10:56:35 +0100

    refactored to mock tiles and board

--- spec/units/game.test.js
+++ spec/units/game.test.js
@@ -1,8 +1,26 @@
 const Game = require('../../lib/game.js')
+const mockTile = jest.createMockFromModule('../../lib/tile.js')
+mockTile.value = jest.fn(() => null)
+
+const mockBoard = jest.fn().mockImplementation(() => {
+  return {
+    tiles: [
+      [mockTile, mockTile, mockTile],
+      [mockTile, mockTile, mockTile],
+      [mockTile, mockTile, mockTile]
+    ]
+  }
+})
 
 describe('Game', () => {
+  beforeEach (() => {
+    mockTile.mockClear()
+    mockBoard.mockClear()
+  })
   it('initialises with an instance of board', () => {
-    const game = new Game()
-    expect(game.board).not.toBeUndefined()
+    const game = new Game(mockBoard)
+    expect(game.board).toBe(mockBoard)
+    const tile = game.board().tiles[0][0]
+    expect(tile.value()).toBe(null)
   })
 })
